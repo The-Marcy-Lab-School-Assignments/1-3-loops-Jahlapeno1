52c7ede30861d34e9349117d578beaa34a79084b
Jahlapeno1 <jimenezaot@gmail.com>
2024-04-09 15:28:14 -0400

    FINISHED

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -40,7 +40,20 @@ const loopUpToNum = (num) => {
 // loopUpToNum(0)
 
 const fizzbuzz = () => {
+  for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log('fizzbuzz')
+    } else if (i % 3 === 0) {
+      console.log('fizz')
+    } else if (i % 5 === 0) {
+      console.log('buzz')
+    } else {
+      console.log(i)
+    }
+  }
 };
+
+fizzbuzz()
 
 module.exports = {
   loop0UpTo10,
